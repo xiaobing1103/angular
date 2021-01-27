d27336b8a70376ece3e5fd6dd01c80489419eb69
xiaobing <https://github.com/xiaobing1103>
2021-01-27 10:33:54 +0800

    feat:数据绑定的第二种方式,以及html模板的优先级 ts中的优先级

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -71,4 +71,8 @@ export class AppComponent {
     console.log(this.username);
     console.log(this.password)
   }
+  getContent(input1:any,input2:any){
+    console.log(input1)
+    console.log(input2)
+  }
 }
